512be412336f91c06aed29db717a26a1b7035936
rkautomation101 <rkudikala83@gmail.com>
2020-02-06 11:30:47 +0000

    First Commit

---
+++ SeleniumAutomation/SeleniumAutomation/SeleniumTest.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumAutomation
+{
+    public class SeleniumTest
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\driver");
+            driver.Url = "http://demowebshop.tricentis.com/";
+            driver.Manage().Window.Maximize();
+            string title = driver.Title;
+            if (title.Equals("Demo Web Shop"))
+            {
+                Console.WriteLine("Test Pass");
+            }
+            else
+            {
+                Console.WriteLine("test Fail");
+            }
+            driver.Quit();
+        }
+    }
+}
